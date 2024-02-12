c8c76b1cae212c13ff88df19ec7174ffbaa292ea
Bryan Francisco <franciscobryaaan@gmail.com>
2024-02-12 17:19:56 +0800

    added urls

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import index, task_list
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('list', task_list, name="list"),
+]
+
+app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World")
+
+def task_list(request):
+    ctx = {
+        "tasks" : [
+            'Task 1',
+            'Task 2', 
+            'Task 3', 
+            'Task 4', 
+        ]
+    }
+
+    return render(request, "task_list.html", ctx)
