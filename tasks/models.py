ea89a99c1007af7916a7d42c6b5b5bd7fab23d91
Bryan Francisco <franciscobryaaan@gmail.com>
2024-02-19 15:19:21 +0800

    migrations session

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    due_date = models.DateTimeField(null=False)
+    taskgroup = models.ForeignKey('TaskGroup', 
+        on_delete=models.CASCADE, 
+        default=1, 
+        related_name='tasks'
+    )
+
+    def __str__(self):
+        return f'{self.name} due on {self.due_date}'
+
+    def get_absolute_url(self):
+        return reverse('task-detail', args=[str(self.pk)])
+
+    @property
+    def is_due(self):
+        return datetime.now() >= self.due_date
+
+    class Meta:
+        ordering = ['due_date']
+        unique_together = [
+            ['due_date', 'name'], 
+        ]
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks' 
+        
+        
