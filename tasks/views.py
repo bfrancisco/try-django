0ea8e67e333e1362215f7f407222b454269fef16
Bryan Francisco <franciscobryaaan@gmail.com>
2024-04-04 19:15:36 +0800

    fixed reverse_lazy error

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 # from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Task, TaskGroup
 from .forms import TaskForm
