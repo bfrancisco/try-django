4d03ad1f884b0fec9b8a07d8f908ea9507ec79ed
Bryan Francisco <franciscobryaaan@gmail.com>
2024-04-04 14:47:11 +0800

    cleaned views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+# from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
 
